38d045169c5699939bb6511ecd7e540564f32527
Elbert <elbert@RTLT57.hitronhub.home>
2021-06-05 09:10:24 -0700

    update encrypt and decrypt to use iv passed through hashed payload

--- packages/webhook-client/webhook-cipher/index.ts
+++ packages/webhook-client/webhook-cipher/index.ts
@@ -2,20 +2,26 @@ import crypto from 'crypto'
 
 export class WebhookCipher {
   private alg: string
-  private iv: any
 
   constructor() {
     this.alg = 'aes-256-cbc'
-    this.iv = '1234567891234567' // crypto.randomBytes(16).toString('hex').slice(0, 16)
   }
 
   public encrypt(message: string, secretKey: string) {
-    const cipher = crypto.createCipheriv(this.alg, secretKey, this.iv)
-    return cipher.update(message, 'utf8', 'hex') + cipher.final('hex')
+    const iv = crypto.randomBytes(16).toString('hex').slice(0, 16)
+    const cipher = crypto.createCipheriv(this.alg, secretKey, iv)
+    return iv + cipher.update(message, 'utf8', 'hex') + cipher.final('hex')
   }
 
   public decrypt(hash: string, secretKey: string) {
-    const decipher = crypto.createDecipheriv(this.alg, secretKey, this.iv)
-    return decipher.update(hash, 'hex', 'utf8') + decipher.final('utf8')
+    const [iv, payload] = this.extractEncryptedPayloadParts(hash)
+    const decipher = crypto.createDecipheriv(this.alg, secretKey, iv)
+    return decipher.update(payload, 'hex', 'utf8') + decipher.final('utf8')
+  }
+
+  private extractEncryptedPayloadParts(hash: string): string[] {
+    const iv = hash.slice(0, 16)
+    const payload = hash.slice(16)
+    return [iv, payload]
   }
 }
--- packages/webhook-server/src/commands/index.ts
+++ packages/webhook-server/src/commands/index.ts
@@ -1,14 +1,13 @@
-export { BaseCommand, CommandContext } from "./base-command";
+export { BaseCommand, CommandContext } from './base-command'
 
-export { CreateOwnerCommand } from "./create-owner-command";
-export { GetOwnerCommand } from "./get-owner-command";
-export { RegenerateTokensCommand } from "./regenerate-tokens-command";
-export { DeleteOwnerCommand } from "./delete-owner-command";
+export { CreateOwnerCommand } from './create-owner-command'
+export { GetOwnerCommand } from './get-owner-command'
+export { RegenerateTokensCommand } from './regenerate-tokens-command'
+export { DeleteOwnerCommand } from './delete-owner-command'
 
-export { TestWebhookCommand } from "./test-webhook-command";
-export { UpdateWebhook } from "./update-webhook-command";
-export { CreateWebhookCommand } from "./create-webhook-command";
-export { GetWebhooksCommand } from "./get-webhooks-command";
-export { DeleteWebhookCommand } from "./delete-webhook-command";
+export { UpdateWebhook } from './update-webhook-command'
+export { CreateWebhookCommand } from './create-webhook-command'
+export { GetWebhooksCommand } from './get-webhooks-command'
+export { DeleteWebhookCommand } from './delete-webhook-command'
 
-export { ProcessWebhookCommand } from "./process-webhook-command";
+export { ProcessWebhookCommand } from './process-webhook-command'
